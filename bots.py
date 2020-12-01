ffb05217dc9a2a2fe8d35b15c454fde6c389e515
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2020-12-01 16:47:35 -0500

    Updated the tron stencil.

--- bots.py
+++ bots.py
@@ -20,7 +20,20 @@ class StudentBot:
         state by calling asp.get_start_state()
         """
         return "U"
+    
+    def cleanup(self):
+        """
+        Input: None
+        Output: None
 
+        This function will be called in between
+        games during grading. You can use it
+        to reset any variables your bot uses during the game
+        (for example, you could use this function to reset a
+        turns_elapsed counter to zero). If you don't need it,
+        feel free to leave it as "pass"
+        """
+        pass
 
 class RandBot:
     """Moves in a random (safe) direction"""
@@ -39,6 +52,9 @@ class RandBot:
         if possibilities:
             return random.choice(possibilities)
         return "U"
+    
+    def cleanup(self):
+        pass
 
 
 class WallBot:
